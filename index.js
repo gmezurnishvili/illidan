875fbe25d62e122f181a7d3cda8d6c34918b5f5a
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 13:41:16 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(__dirname + '/'));
- 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+app.use(express.static('views'));
 
 app.get('/api', function(req, res) {
   res.json({data: "Hello World"});
 })
 
 app.get('/view', function(req, res) {
-  res.render('index');
+  res.render('index.html');
 })
 
 app.listen(app.get('port'), function() {
