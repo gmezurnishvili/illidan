b2d4e9862001709725ed70c215662aaa15631457
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 13:37:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/views'));
+app.use('/', express.static(__dirname + '/'));
  
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
