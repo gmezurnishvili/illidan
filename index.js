804ad96c3efc02bfad6da28a979a3ddd6e3819ea
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 12:54:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
  
 var app = express()
+
+app.set('port', (process.env.PORT || 5000));
  
 app.get('/notes', function(req, res) {
   res.json({notes: "Hello World"})
 })
- 
-app.listen(3000)
