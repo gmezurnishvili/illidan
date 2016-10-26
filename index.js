96191b12615e4f74df4a83f92dcdee561772f84a
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 13:33:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/views'));
  
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
