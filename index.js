8dd5aac8bd2e9024a798fe39b00db808d16e889b
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 13:30:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/api', function(req, res) {
 })
 
 app.get('/view', function(req, res) {
-  res.render('views/index.html');
+  res.render('views/index');
 })
 
 app.listen(app.get('port'), function() {
