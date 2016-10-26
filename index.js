2418b26d9c063e75726071abc9fddd9813781ac9
gmezurnishvili <gmezurnishvili@gmail.com>
2016-10-26 13:07:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/notes', function(req, res) {
   res.json({notes: "Hello World"})
 })
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
