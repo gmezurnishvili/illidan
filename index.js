b5d81d3859a18f0fdcd96d3592df979818c925c3
gmezurnishvili <gmezurnishvili@gmail.com>
2016-11-07 21:42:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -232,8 +232,7 @@ function getSenderData(recipientId) {
     
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(response);
-      console.log(body);
+      console.log(body.first_name);
     } else {
       console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
     }
