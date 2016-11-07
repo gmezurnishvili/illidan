3b6c11776d31cf0f9dbcb3dd2f3ea0a4a2b3e8c3
gmezurnishvili <gmezurnishvili@gmail.com>
2016-11-07 13:59:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
-var express = require('express')
-var guid = require('guid')
-var mongoose = require('mongoose');
-
+const 
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),  
+  request = require('request');
+  guid = require('guid');
 
 var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/static', express.static(__dirname + '/views'));
+//app.use('/static', express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
+//app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+console.log(config);
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+ 
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+ 
+ // Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+  
+ // URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
+ 
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+	console.log(req.query['hub.mode']) ;
+	console.log(req.query['hub.verify_token']) ;
+	console.log(VALIDATION_TOKEN) ;
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+  
+  
 
 app.get('/api', function(req, res) {
 
   res.json({data: "Hello World",id:guid.raw()});
 })
 
-app.get('/index', function(req, res) {
-  res.sendFile('/views/index.html');
-})
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
