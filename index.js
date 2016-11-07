d17fddba407016462d9e9f076bb3f3ecc525c7b9
gmezurnishvili <gmezurnishvili@gmail.com>
2016-11-07 16:36:13 +0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
 	console.log("Request is ");
-		console.log(req);
+		console.log(req.body);
 		return;
   var data = req.body;
 
